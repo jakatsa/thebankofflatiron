ceba5d8ec968e53a256d2410993503d0eb89e5ca
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-02-15 14:43:38 +0300

    Update AddTransactionForm.js

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,72 +1,33 @@
-import { useState } from "react";
+import React from "react";
+import AddTransactionForm from "./AddTransactionForm";
 
-export default function AddTransactionForm({ onAdd }) {
-  const [newTransaction, setNewTransaction] = useState({
-    description: "",
-    amount: "",
-    date: "",
-    category: "",
-  });
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    //object property shorthand syntax :  [name]: value
-    // dynamically sets a property on the object
-    setNewTransaction({ ...newTransaction, [name]: value });
-  };
+function AddTransaction(){
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onAdd(newTransaction);
-    // resetting the input box
-    setNewTransaction({ description: "", amount: "", date: "", category: "" });
-  };
-  return (
-    <>
-      <div className="container">
-        <h2>Add Transaction</h2>
-        <form className="form-control" onSubmit={handleSubmit}>
-          <label htmlFor="description">Description</label>
-          <input
-            type="text"
-            className="form-control"
-            id="description"
-            name="description"
-            value={newTransaction.description}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            id="amount"
-            className="form-control"
-            name="amount"
-            value={newTransaction.amount}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            className="form-control"
-            id="date"
-            name="date"
-            value={newTransaction.date}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="category">Category</label>
-          <input
-            type="text"
-            className="form-control"
-            id="category"
-            name="category"
-            value={newTransaction.category}
-            onChange={handleInputChange}
-          />
-          <br></br>
-          <button type="submit" className="btn btn-success">
-            Add new transaction
-          </button>
-        </form>
-      </div>
-    </>
-  );
+    const addTransaction = async (newTransaction) => {
+        try {
+            const response = await fetch("https://json-server-vercel-seven-tau.vercel.app/transactions", {
+               method: 'POST',
+               headers: {
+                'Content-Type' : 'application/json'
+               },
+               body: JSON.stringify(newTransaction)
+            });
+            if(response.ok){
+                  //re render 
+                  fetchTransaction(); 
+            }else {
+                 console.log('Error adding transaction ' , response.statusText)
+            }
+        }catch(error) {
+          console.error("error adding transaction " , error)
+        }
+    }
+
+    return (
+        <div>
+            <AddTransactionForm onAdd={addTransaction}/>
+        </div>
+    )
 }
+
+export default AddTransaction;
