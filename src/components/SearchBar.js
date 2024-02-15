97d25c9487d94390fb55c5cb99c2de4e10e80398
jakatsa <118554412+jakatsa@users.noreply.github.com>
2024-02-15 14:46:19 +0300

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,31 @@
-import { useState } from "react";
+import { useState } from "react"
 
-export default function SearchBar({ onSearch }) {
+export default function SearchBar({onSearch}){
   //state to save the inputs text from the input box
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleInputChange = (event) => {
-    //  console.log(event.target.value)
-    //capture forms input
-    const value = event.target.value;
-    setSearchTerm(value);
-    //child back to parent communication
-    onSearch(searchTerm);
-  };
+      //  console.log(event.target.value)
+          //capture forms input
+         const value = event.target.value;
+         setSearchTerm(value)
+         //child back to parent communication
+         onSearch(searchTerm)
 
-  return (
-    <>
-      <div>
-        <label htmlFor="search">Search:</label>
-        <input
+  }
+
+    return (
+       <>
+         <div>
+          <label htmlFor="search">Search:</label>
+          <input
           type="text"
           id="search"
           value={searchTerm}
-          onChange={handleInputChange}
-          placeholder="Search Description"
-        />
-      </div>
-    </>
-  );
+          onChange={handleInputChange}    
+          placeholder="Search Description"      
+          />
+         </div>
+       </>
+    )
 }
